6dc8cd6f83f162fb9836145194bc7ea8a07e7df7
Ana Júlia Volpi <anajuliavolpi45@gmail.com>
2023-02-20 19:52:19 -0300

    Initial project commit

---
+++ MeuAtelieApp/MeuAtelieApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeuAtelieApp
+//
+//  Created by Ana Júlia Volpi on 20/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Olá, MeuAtelieApp!")
+                .font(.system(size: 32))
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeuAtelieApp/MeuAtelieApp/MeuAtelieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeuAtelieAppApp.swift
+//  MeuAtelieApp
+//
+//  Created by Adolpho Piazza on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MeuAtelieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
