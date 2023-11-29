8152bf727efe9aba6d079d81bee709a7c4764698
Ana Júlia Volpi <anajuliavolpi45@gmail.com>
2023-11-29 20:26:42 -0300

    Allow user to save profile picture when creating account

--- MeuAtelieApp/MeuAtelieApp/MeuAtelieUI/Views/Register/MARegisterView.swift
+++ MeuAtelieApp/MeuAtelieApp/MeuAtelieUI/Views/Register/MARegisterView.swift
@@ -6,15 +6,23 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct MARegisterView: View {
-    
     @ObservedObject var viewModel: MARegisterViewModel = MARegisterViewModel()
     @EnvironmentObject var networkManager: NetworkManager
+    
     @State var emailAddress: String = ""
     @State var password: String = ""
     @State var firstName: String = ""
     @State var lastName: String = ""
+    @State var userImage: Image?
+    
+    @State var showingActionSheet: Bool = false
+    @State var showingImagePicker: Bool = false
+    @State var showingCameraPicker: Bool = false
+    @State var imagePicked: PhotosPickerItem?
+    @State var uiImage: UIImage?
     
     var body: some View {
         ZStack {
@@ -44,7 +52,40 @@ struct MARegisterView: View {
             }
         }
         .addMALoading(state: viewModel.isLoading)
+        .addMAError(state: viewModel.showError,
+                    message: "Cadastro incorreto", action: {
+            viewModel.showError = false
+        })
         .hideKeyboard()
+        .confirmationDialog("Tirar uma foto ou selecionar da galeria", isPresented: $showingActionSheet, actions: {
+            Button("Escolher da galeria") {
+                showingImagePicker = true
+            }
+            
+            Button("Tirar foto") {
+                showingCameraPicker = true
+            }
+        })
+        .photosPicker(isPresented: $showingImagePicker, selection: $imagePicked, matching: .images)
+        .fullScreenCover(isPresented: $showingCameraPicker) {
+            CameraPicker(sourceType: .camera) { image in
+                self.userImage = Image(uiImage: image)
+                self.uiImage = image
+            }
+        }
+        .onChange(of: imagePicked) { _, _ in
+            Task {
+                if let data = try? await imagePicked?.loadTransferable(type: Data.self) {
+                    if let uiImage = UIImage(data: data) {
+                        self.userImage = Image(uiImage: uiImage)
+                        self.uiImage = uiImage
+                        return
+                    }
+                }
+                
+                print("Failed to get image from imagePicker")
+            }
+        }
     }
     
     var backgroundView: some View {
@@ -81,6 +122,44 @@ struct MARegisterView: View {
                 .textContentType(.name)
                 .autocorrectionDisabled()
                 .padding(.top, 14)
+            
+            HStack(spacing: 30) {
+                if let userImage {
+                    userImage
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                        .frame(width: 116, height: 113)
+                        .padding()
+                } else {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 8)
+                            .foregroundColor(.MAColors.MAImageGray)
+                            .frame(width: 116, height: 113)
+                        
+                        Image.MAImages.Login.loginTopImage
+                            .resizable()
+                            .foregroundStyle(.white)
+                            .frame(width: 100, height: 100)
+                    }
+                    .padding()
+                }
+                
+                Text("Inserir foto de perfil")
+                    .font(.system(size: 18, weight: .semibold))
+                    .foregroundStyle(Color.MAColors.MAPinkStrong)
+                    .multilineTextAlignment(.center)
+                    .padding(.trailing, 20)
+            }
+            .frame(maxWidth: .infinity)
+            .background {
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundStyle(.white)
+            }
+            .padding(.top, 14)
+            .onTapGesture {
+                showingActionSheet = true
+            }
         }
     }
     
@@ -90,7 +169,10 @@ struct MARegisterView: View {
                                         password: self.password,
                                         firstName: self.firstName,
                                         lastName: self.lastName)
-            viewModel.signUpWith(networkManager, and: model)
+            
+            Task {
+                await viewModel.createUser(networkManager, model: model, image: uiImage ?? UIImage())
+            }
         }
         .buttonStyle(MABasicButtonStyle())
     }
